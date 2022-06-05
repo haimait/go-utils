a6c6d5564f7220422c00e227365cc81b91c088d1
wanghaima <whm2416@qq.com>
2022-06-05 09:13:51 +0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/haimait/go-utils
+
+go 1.18
---
+++ uuid/uuid_test.go
@@ -0,0 +1,72 @@
+package uuid
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestUUID(t *testing.T) {
+	uuid, err := NewUUID()
+	fmt.Printf("err:%v uuid:%v \n", err, uuid)
+
+	uuidStr := uuid.String()
+	fmt.Printf("uuidStr:%v\n", uuidStr)
+
+	uuidCopy := uuid.Copy()
+	fmt.Printf("uuidCopy:%v\n", uuidCopy)
+
+	uuidRaw := uuid.Raw()
+	fmt.Printf("uuidRaw:%v\n", uuidRaw)
+
+}
+
+func TestUUIDFromString(t *testing.T) {
+	_, err := UUIDFromString("blah")
+	fmt.Println("err:", err)
+	validUUID := "9f484882-2f18-4fd2-967d-db9663db7bea"
+	uuid, err := UUIDFromString(validUUID)
+	fmt.Printf("uuid:%v err:%v \n", uuid, err)
+
+}
+
+func TestIsValidUUIDString(t *testing.T) {
+
+	tests := []struct {
+		args string
+		want bool
+	}{
+		{
+			UUID{}.String(),
+			true,
+		},
+		{
+			"",
+			false,
+		},
+		{
+			"blah",
+			false,
+		},
+		{
+			"blah-9f484882-2f18-4fd2-967d-db9663db7bea",
+			false,
+		},
+		{
+			"9f484882-2f18-4fd2-967d-db9663db7bea-blah",
+			false,
+		},
+		{
+			"9f484882-2f18-4fd2-967d-db9663db7bea",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.args, func(t *testing.T) {
+			got := IsValidUUIDString(tt.args)
+			fmt.Println("got:", got)
+			if got != tt.want {
+				t.Errorf("IsValidUUIDString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
